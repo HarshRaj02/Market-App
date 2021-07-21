6c954176a17566023f87da6d4b618a0260147821
Harsh Raj <harsh.raj.02@tatahealth.com>
2021-07-21 16:55:54 +0530

    First commit in Market App

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Everything Available Here! Visit and Revisit!</h2>
+      <p>
+        Choose your desired products from our broad selection of assorted items
+        and enjoy a satisfactory experience of fulfilled expectations.
+      </p>
+      <p>
+        All our goods are processed with high-quality care, just-in-time and
+        of course by experienced professionals!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
